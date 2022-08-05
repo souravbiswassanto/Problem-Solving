8154979b51bab73e93087c657593af6aae00b6f5
souravbiswassanto <48715910+souravbiswassanto@users.noreply.github.com>
2022-08-06 01:20:47 +0600

    Create k (segment Tree + binary Search).cpp

---
+++ vjudge/32/k (segment Tree + binary Search).cpp
@@ -0,0 +1,123 @@
+// Problem: K - K
+// Contest: Virtual Judge - ICPC Collaborative Contest - 31
+// URL: https://vjudge.net/contest/508114#problem/K
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable" 
+#pragma GCC diagnostic ignored "-Wformat"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+//#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std ;
+
+#define   Pi     2*acos(0.0)
+#define   ll     long long
+#define   pb     push_back
+#define   mp     make_pair
+#define   MAX    500006
+#define   MAX1   10000008
+#define   mem(a,v)   memset(a,v,sizeof(a))
+#define   lcm(a, b)        ((a)*((b)/__gcd(a,b)))
+#define yes   cout<<"YES"<<endl
+#define no   cout<<"NO"<<endl
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());	
+template<class T> using oset=tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// order_of_key returns the 0 based position of a value
+// find_by_order returns a pointer pointing at the kth element 
+// also 0 indexed
+ll A[MAX], B[MAX], C[MAX],k;
+
+struct SegmentTree{
+	vector<ll> tree;
+	int n;
+	SegmentTree(int _n){
+	    n = _n;
+	    tree.clear();
+	    tree.resize(n * 4);
+	}
+	void build(int node, int tb, int te){
+	    if(tb > te) return;
+		if(tb == te){
+			tree[node]  = (A[tb] % k);//value goes here
+			return;
+		}
+		int mid=(tb + te) >> 1; 
+		int left = (node * 2);
+		int right = node * 2 + 1;
+		build(left, tb, mid);
+		build(right, mid + 1, te);
+		tree[node] = (tree[left] * 1LL * tree[right]) % k;
+	}
+	
+	ll query(int node, int tb, int te, int qb, int qe){
+        if(tb > te) return 1;
+        if(qb > te || qe < tb)return 1;
+        if(qb <= tb && te <= qe){
+            return tree[node];
+        }
+        int left, right, mid;
+        left = (node << 1);
+        right = left + 1;
+        mid = (tb + te) >> 1;
+        ll L = query(left, tb, mid, qb, qe);
+        ll R = query(right, mid + 1, te, qb, qe);
+        return (L * 1LL * R) % k;
+    }
+
+}; 
+
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    
+    ll a, b, c, d, n, m,  l, p, q, r, t, x, y;
+    ll cnt = 0, cntr = 0, sum = 0,ans = 1, check = 0;
+    ll Max = 0, Min = 2e18;
+    string s, s1, s2;
+
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++) {
+    	cin >> A[i];
+    }
+    SegmentTree st(n);
+    st.build(1, 1, n);
+    
+    for(int i = 1; i <= n; i++) {
+    	l = i; r = n;
+    	
+    	while(l <= r){
+    		ll mid = (l + r) / 2;
+    		
+    		a = st.query(1, 1, n, i, mid);
+    		//cout << l <<" " << mid <<" " << r <<" " << a << endl;
+    		if(a){
+    			l = mid + 1;
+    		}
+    		else r = mid - 1;
+    	}
+    	//cout << l << endl;
+    	sum += (n - l + 1);
+    }
+    cout << sum << endl;
+    
+    /* 
+        precedence : ! * / % + - << >> 
+        < > <= >= == != & ^ | && ||
+        always check the limit and make sure that is correct.
+        lcm of first 40 numbers  can be calculated.
+        sqrt of 10^9 is 31622.
+        always usee 1LL while doing binary operations.
+    */
+
+    return (0 - 0 + 1 -  1 * 1);
+}
